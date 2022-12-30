c36104a3346e3bfad9682c2400c22efdb60b9306
asluchych <anatolblr@gmail.com>
2022-12-30 13:32:00 +0100

    Implement game end

--- tetris.py
+++ tetris.py
@@ -8,6 +8,7 @@ class Tetris:
         self.sprite_group = pg.sprite.Group()
         self.field_array = self.get_field_array()
         self.tetromino = Tetromino(self)
+        self.next_tetromino = Tetromino(self, current=False)
         self.speed_up = False
 
     def check_full_line(self):
@@ -35,12 +36,21 @@ class Tetris:
     def get_field_array(self):
         return [[0 for x in range(FIELD_W)] for y in range(FIELD_H)]
 
+    def is_game_over(self):
+        if self.tetromino.blocks[0].pos.y == INIT_POS_OFFSET[1]:
+            pg.time.wait(300)
+            return True
 
     def check_tetromino_landing(self):
         if self.tetromino.landing:
-            self.speed_up = False
-            self.put_tetromino_blocks_in_array()
-            self.tetromino = Tetromino(self)
+            if self.is_game_over():
+                self.__init__(self.app)
+            else:
+                self.speed_up = False
+                self.put_tetromino_blocks_in_array()
+                self.next_tetromino.current = True
+                self.tetromino = self.next_tetromino
+                self.next_tetromino = Tetromino(self, current=False)
 
 
     def control(self, pressed_key):
