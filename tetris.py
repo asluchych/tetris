74dccecfbea8b1662ff7df6a7719d8d66979e078
asluchych <anatolblr@gmail.com>
2022-12-30 16:12:38 +0100

    Add text

--- tetris.py
+++ tetris.py
@@ -1,6 +1,23 @@
 from settings import *
 import math
 from tetromino import Tetromino
+import pygame.freetype as ft
+
+class Text:
+    def __init__(self, app):
+        self.app = app
+        self.font = ft.Font(FONT_PATH)
+
+
+    def draw(self):
+        self.font.render_to(self.app.screen, (WIN_W * 0.595, WIN_H * 0.02),
+                            text='TETRIS', fgcolor='white', size=TILE_SIZE * 1.65, bgcolor='black')
+        self.font.render_to(self.app.screen, (WIN_W * 0.65, WIN_H * 0.22),
+                            text='next', fgcolor='orange', size=TILE_SIZE * 1.4, bgcolor='black')
+        self.font.render_to(self.app.screen, (WIN_W * 0.64, WIN_H * 0.67),
+                            text='score', fgcolor='orange', size=TILE_SIZE * 1.4, bgcolor='black')
+        self.font.render_to(self.app.screen, (WIN_W * 0.64, WIN_H * 0.8),
+                            text='00', fgcolor='white', size=TILE_SIZE * 1.8)
 
 class Tetris:
     def __init__(self, app):
